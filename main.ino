d4d2f3bb28a9f99341b44d071644a691e9f20cba
Alan Volker <volkeralan12@gmail.com>
2022-04-05 18:43:24 -0300

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,24 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/*  
+        Alarma Sanbo
+%%%%%%%%%%%%%   Autores   %%%%%%%%%%%%%%%
+-Alan Völker 
+-Federico Chara Lobo 
+-Leandro Ponte Recalde 
+-Ezequiel Santangelo
+%%%%%%%%%%%%%   Materiales   %%%%%%%%%%%%%%%
+##
+%%%%%%%%%%%%%   Licencia   %%%%%%%%%%%%%%%
+*/
+
+void setup()
+{
+    Serial.begin(9600);
+    
+}
+void loop()
+{
+    
+}
